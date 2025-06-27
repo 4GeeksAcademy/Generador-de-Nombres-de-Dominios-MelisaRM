882ce57e59a4d917332287bcba3e4259aa5f52d8
Melisa Rodero Molano <melisa.95.mr@gmail.com>
2025-06-27 07:26:31 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
+
+let pronoun = ['nosotros', 'vosotros'];
+let adj = ['grande', 'pequeño'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.us', '.io'];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extensions.length; l++) {
+        let domain = pronoun[i] + adj[j] + noun[k] + extensions[l];
+        console.log(domain);
+      }
+    }
+  }
+}
