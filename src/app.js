1701ce02389673eeab4ba3439312b5a681ee8232
Melisa Rodero Molano <melisa.95.mr@gmail.com>
2025-06-30 13:15:34 +0000

    cambios2

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("generarBtn").addEventListener("click", generarDominios);
+});
 
-let pronoun = ['nosotros', 'vosotros'];
-let adj = ['grande', 'pequeño'];
-let noun = ['jogger', 'racoon'];
-let extensions = ['.com', '.net', '.us', '.io'];
+function generarDominios() {
+  let pronoun = ['nosotros', 'vosotros'];
+  let adj = ['grande', 'pequeño'];
+  let noun = ['jogger', 'racoon'];
+  let extensions = ['.com', '.net', '.us', '.io'];
 
+  let resultado = '';
 
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
-      for (let l = 0; l < extensions.length; l++) {
-        let domain = pronoun[i] + adj[j] + noun[k] + extensions[l];
-        console.log(domain);
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          let domain = pronoun[i] + adj[j] + noun[k] + extensions[l];
+          resultado += domain + '<br>';
+        }
       }
     }
   }
+
+  document.getElementById("resultado").innerHTML = resultado;
 }
+
